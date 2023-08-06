a0a95dadc0a04da09c04539971f9cb276797e98c
Vitaliy Melnychuk <78220097+Vitaliy-Melnychuk@users.noreply.github.com>
2023-08-06 22:53:31 +0100

    add photos

---
+++ script/main.js
@@ -0,0 +1,296 @@
+const categories = {
+    mainGallery: [
+        {name: 's2021_001', data: 'id="s2021_001"', description: '', descriptionStyle: 's2021_001', folder: 's2021'},
+        {name: 's2021_002', data: 'id="s2021_002"', description: '', descriptionStyle: 's2021_002', folder: 's2021'},
+        {name: 's2021_003', data: 'id="s2021_003"', description: '', descriptionStyle: 's2021_003', folder: 's2021'},
+        {name: 's2021_004', data: 'id="s2021_004"', description: '', descriptionStyle: 's2021_004', folder: 's2021'},
+        {name: 's2021_005', data: 'id="s2021_005"', description: '', descriptionStyle: 's2021_005', folder: 's2021'},
+        {name: 's2021_006', data: 'id="s2021_006"', description: '', descriptionStyle: 's2021_006', folder: 's2021'},
+        {name: 's2021_007', data: 'id="s2021_007"', description: '', descriptionStyle: 's2021_007', folder: 's2021'},
+        {name: 's2021_008', data: 'id="s2021_008"', description: '', descriptionStyle: 's2021_008', folder: 's2021'},
+        {name: 's2021_009', data: 'id="s2021_009"', description: '', descriptionStyle: 's2021_009', folder: 's2021'},
+        {name: 's2021_010', data: 'id="s2021_010"', description: '', descriptionStyle: 's2021_010', folder: 's2021'},
+    ],
+    wedding: [
+        {name: 's2022_001', data: 'id="s2022"', description: 'Колекция 2022<span class="catalog__description-letter">г</span>', descriptionStyle: 'Свадебные платья', folder: 's2022'},
+        {name: 's2021_001', data: 'id="s2021"', description: 'Колекция 2021<span class="catalog__description-letter">г</span>', descriptionStyle: 'Свадебные платья', folder: 's2021'},
+        {name: 's2020_001', data: 'id="s2020"', description: 'Колекция 2020<span class="catalog__description-letter">г</span>', descriptionStyle: 'Свадебные платья', folder: 's2020'},
+    ],
+    evening: [
+        {name: 'pl5017', data: 'id="plvs"', description: 'Колекция (s)<span class="catalog__description-letter"></span>', descriptionStyle: 'Плотна вишивка', folder: 'plvs'},
+        {name: 'pl1502', data: 'id="plvx"', description: 'Колекция (x)<span class="catalog__description-letter"></span>', descriptionStyle: 'Плотна вишивка', folder: 'plvx'},
+        {name: 'pl1510', data: 'id="plv3x"', description: 'Колекция (3x)<span class="catalog__description-letter"></span>', descriptionStyle: 'Плотна вишивка', folder: 'plv3x'},
+    ],
+    children: [
+        {name: 'v2020_001', data: 'id="v2020"', description: 'Колекция 2020<span class="catalog__description-letter">г</span>', descriptionStyle: 'Вечерние платья', folder: 'v2020'},
+        {name: 'v2020_001', data: 'id="v2020"', description: 'Колекция 2020<span class="catalog__description-letter">г</span>', descriptionStyle: 'Вечерние платья', folder: 'v2020'},
+        {name: 'v2021_001', data: 'id="v2021"', description: 'Колекция 2021<span class="catalog__description-letter">г</span>', descriptionStyle: 'Вечерние платья', folder: 'v2021'},
+    ],
+    accessories: [
+        {name: 'v2020_001', data: 'id="v2020"', description: 'Колекция 2020<span class="catalog__description-letter">г</span>', descriptionStyle: 'Вечерние платья', folder: 'v2020'},
+        {name: 'v2020_001', data: 'id="v2020"', description: 'Колекция 2020<span class="catalog__description-letter">г</span>', descriptionStyle: 'Вечерние платья', folder: 'v2020'},
+        {name: 'v2021_001', data: 'id="v2021"', description: 'Колекция 2021<span class="catalog__description-letter">г</span>', descriptionStyle: 'Вечерние платья', folder: 'v2021'},
+    ],
+    about: [
+        {name: 'v2020_001', data: 'id="v2020"', description: 'Колекция 2020<span class="catalog__description-letter">г</span>', descriptionStyle: 'Вечерние платья', folder: 'v2020'},
+        {name: 'v2020_001', data: 'id="v2020"', description: 'Колекция 2020<span class="catalog__description-letter">г</span>', descriptionStyle: 'Вечерние платья', folder: 'v2020'},
+        {name: 'v2021_001', data: 'id="v2021"', description: 'Колекция 2021<span class="catalog__description-letter">г</span>', descriptionStyle: 'Вечерние платья', folder: 'v2021'},
+    ],
+    shares: [
+        {name: 'v2020_001', data: 'id="v2020"', description: 'Колекция 2020<span class="catalog__description-letter">г</span>', descriptionStyle: 'Вечерние платья', folder: 'v2020'},
+        {name: 'v2020_001', data: 'id="v2020"', description: 'Колекция 2020<span class="catalog__description-letter">г</span>', descriptionStyle: 'Вечерние платья', folder: 'v2020'},
+        {name: 'v2021_001', data: 'id="v2021"', description: 'Колекция 2021<span class="catalog__description-letter">г</span>', descriptionStyle: 'Вечерние платья', folder: 'v2021'},
+    ],
+    plv3x: [
+        {name: 'pl1503', data: 'id="pl1503"', description: '', descriptionStyle: '#1503', folder: 'plv3x'},
+        {name: 'pl1504', data: 'id="pl1504"', description: '', descriptionStyle: '#1504', folder: 'plv3x'},
+        {name: 'pl1505', data: 'id="pl1505"', description: '', descriptionStyle: '#1505', folder: 'plv3x'},
+        {name: 'pl1507', data: 'id="pl1507"', description: '', descriptionStyle: '#1507', folder: 'plv3x'},
+        {name: 'pl1508', data: 'id="pl1508"', description: '', descriptionStyle: '#1508', folder: 'plv3x'},
+        {name: 'pl1509', data: 'id="pl1509"', description: '', descriptionStyle: '#1509', folder: 'plv3x'},
+        {name: 'pl1510', data: 'id="pl1510"', description: '', descriptionStyle: '#1510', folder: 'plv3x'},
+        {name: 'pl1511', data: 'id="pl1511"', description: '', descriptionStyle: '#1511', folder: 'plv3x'},
+        {name: 'pl1512', data: 'id="pl1512"', description: '', descriptionStyle: '#1512', folder: 'plv3x'},
+        {name: 'pl1513', data: 'id="pl1513"', description: '', descriptionStyle: '#1513', folder: 'plv3x'},
+        {name: 'pl1515', data: 'id="pl1515"', description: '', descriptionStyle: '#1515', folder: 'plv3x'},
+        {name: 'pl1516', data: 'id="pl1516"', description: '', descriptionStyle: '#1516', folder: 'plv3x'},
+        {name: 'pl1517', data: 'id="pl1517"', description: '', descriptionStyle: '#1517', folder: 'plv3x'},
+        {name: 'pl1518', data: 'id="pl1518"', description: '', descriptionStyle: '#1518', folder: 'plv3x'},
+        {name: 'pl1519', data: 'id="pl1519"', description: '', descriptionStyle: '#1519', folder: 'plv3x'},
+        {name: 'pl1520', data: 'id="pl1520"', description: '', descriptionStyle: '#1520', folder: 'plv3x'},
+        {name: 'pl1525', data: 'id="pl1525"', description: '', descriptionStyle: '#1525', folder: 'plv3x'},
+        {name: 'pl1527', data: 'id="pl1527"', description: '', descriptionStyle: '#1527', folder: 'plv3x'},
+        {name: 'pl5002', data: 'id="pl5002"', description: '', descriptionStyle: '#5002', folder: 'plv3x'},
+        {name: 'pl5004', data: 'id="pl5004"', description: '', descriptionStyle: '#5004', folder: 'plv3x'},
+        {name: 'pl5018', data: 'id="pl5018"', description: '', descriptionStyle: '#5018', folder: 'plv3x'},
+        {name: 'pl5025', data: 'id="pl5025"', description: '', descriptionStyle: '#5025', folder: 'plv3x'},
+        {name: 'pl5027', data: 'id="pl5027"', description: '', descriptionStyle: '#5027', folder: 'plv3x'},
+        {name: 'pl5028', data: 'id="pl5028"', description: '', descriptionStyle: '#5028', folder: 'plv3x'},
+        {name: 'pl5031', data: 'id="pl5031"', description: '', descriptionStyle: '#5031', folder: 'plv3x'},
+        {name: 'pl5033', data: 'id="pl5033"', description: '', descriptionStyle: '#5033', folder: 'plv3x'},
+        {name: 'pl5038', data: 'id="pl5038"', description: '', descriptionStyle: '#5038', folder: 'plv3x'},
+        {name: 'pl5043', data: 'id="pl5043"', description: '', descriptionStyle: '#5043', folder: 'plv3x'},
+        {name: 'pl5044', data: 'id="pl5044"', description: '', descriptionStyle: '#5044', folder: 'plv3x'},
+        {name: 'pl5045', data: 'id="pl5045"', description: '', descriptionStyle: '#5045', folder: 'plv3x'},
+        {name: 'pl5047', data: 'id="pl5047"', description: '', descriptionStyle: '#5047', folder: 'plv3x'},
+    ],
+    plvx: [
+        {name: 'pl1501', data: 'id="pl1501"', description: '', descriptionStyle: '#1503', folder: 'plvx'},
+        {name: 'pl1502', data: 'id="pl1502"', description: '', descriptionStyle: '#1504', folder: 'plvx'},
+        {name: 'pl1506', data: 'id="pl1506"', description: '', descriptionStyle: '#1505', folder: 'plvx'},
+        {name: 'pl1521', data: 'id="pl1521"', description: '', descriptionStyle: '#1521', folder: 'plvx'},
+        {name: 'pl1522', data: 'id="pl1522"', description: '', descriptionStyle: '#1522', folder: 'plvx'},
+        {name: 'pl5001', data: 'id="pl5001"', description: '', descriptionStyle: '#5001', folder: 'plvx'},
+        {name: 'pl5011', data: 'id="pl5011"', description: '', descriptionStyle: '#5011', folder: 'plvx'},
+        {name: 'pl5012', data: 'id="pl5012"', description: '', descriptionStyle: '#5012', folder: 'plvx'},
+        {name: 'pl5013', data: 'id="pl5013"', description: '', descriptionStyle: '#5013', folder: 'plvx'},
+        {name: 'pl5014', data: 'id="pl5014"', description: '', descriptionStyle: '#5014', folder: 'plvx'},
+        {name: 'pl5015', data: 'id="pl5015"', description: '', descriptionStyle: '#5015', folder: 'plvx'},
+        {name: 'pl5016', data: 'id="pl5016"', description: '', descriptionStyle: '#5016', folder: 'plvx'},
+        {name: 'pl5019', data: 'id="pl5019"', description: '', descriptionStyle: '#5019', folder: 'plvx'},
+        {name: 'pl5020', data: 'id="pl5020"', description: '', descriptionStyle: '#5020', folder: 'plvx'},
+        {name: 'pl5022', data: 'id="pl5022"', description: '', descriptionStyle: '#5022', folder: 'plvx'},
+        {name: 'pl5023', data: 'id="pl5023"', description: '', descriptionStyle: '#5023', folder: 'plvx'},
+        {name: 'pl5024', data: 'id="pl5024"', description: '', descriptionStyle: '#5024', folder: 'plvx'},
+        {name: 'pl5026', data: 'id="pl5026"', description: '', descriptionStyle: '#5026', folder: 'plvx'},
+        {name: 'pl5029', data: 'id="pl5029"', description: '', descriptionStyle: '#5029', folder: 'plvx'},
+        {name: 'pl5030', data: 'id="pl5030"', description: '', descriptionStyle: '#5030', folder: 'plvx'},
+        {name: 'pl5032', data: 'id="pl5032"', description: '', descriptionStyle: '#5032', folder: 'plvx'},
+        {name: 'pl5034', data: 'id="pl5034"', description: '', descriptionStyle: '#5034', folder: 'plvx'},
+        {name: 'pl5037', data: 'id="pl5037"', description: '', descriptionStyle: '#5037', folder: 'plvx'},
+        {name: 'pl5040', data: 'id="pl5040"', description: '', descriptionStyle: '#5040', folder: 'plvx'},
+        {name: 'pl5041', data: 'id="pl5041"', description: '', descriptionStyle: '#5041', folder: 'plvx'},
+        {name: 'pl5042', data: 'id="pl5042"', description: '', descriptionStyle: '#5042', folder: 'plvx'},
+    ],
+    plvs: [
+        {name: 'pl5017', data: 'id="pl5017"', description: '', descriptionStyle: '#5017', folder: 'plvs'},
+        {name: 'pl5035', data: 'id="pl5035"', description: '', descriptionStyle: '#5035', folder: 'plvs'},
+        {name: 'pl5036', data: 'id="pl5036"', description: '', descriptionStyle: '#5036', folder: 'plvs'},
+        {name: 'pl5039', data: 'id="pl5039"', description: '', descriptionStyle: '#5039', folder: 'plvs'},
+    ],
+    v2021: [
+        {name: 'v2021_001', data: 'id="v2021_001"', description: '150$', descriptionStyle: 'v2021_001', folder: 'v2021'},
+        {name: 'v2021_002', data: 'id="v2021_002"', description: '160$', descriptionStyle: 'v2021_002', folder: 'v2021'},
+        {name: 'v2021_007', data: 'id="v2021_007"', description: '170$', descriptionStyle: 'v2021_007', folder: 'v2021'},
+        {name: 'v2021_004', data: 'id="v2021_004"', description: '180$', descriptionStyle: 'v2021_004', folder: 'v2021'},
+        {name: 'v2021_005', data: 'id="v2021_005"', description: '180$', descriptionStyle: 'v2021_005', folder: 'v2021'},
+        {name: 'v2021_006', data: 'id="v2021_006"', description: '180$', descriptionStyle: 'v2021_006', folder: 'v2021'},
+    ],
+    s2020: [
+        {name: 's2020_001', data: 'id="s2020_001"', description: '150$', descriptionStyle: 'Свадебные платья', folder: 's2020'},
+        {name: 's2020_002', data: 'id="s2020_002"', description: '160$', descriptionStyle: 'Свадебные платья', folder: 's2020'},
+        {name: 's2020_003', data: 'id="s2020_003"', description: '170$', descriptionStyle: 'Свадебные платья', folder: 's2020'},
+        {name: 's2020_004', data: 'id="s2020_004"', description: '180$', descriptionStyle: 'Свадебные платья', folder: 's2020'},
+        {name: 's2020_005', data: 'id="s2020_005"', description: '180$', descriptionStyle: 'Свадебные платья', folder: 's2020'},
+    ],
+    v2020: [
+        {name: 'v2020_001', data: 'id="v2020_001"', description: '150$', descriptionStyle: 'Свадебные платья', folder: 'v2020'},
+        {name: 'v2020_002', data: 'id="v2020_002"', description: '160$', descriptionStyle: 'Свадебные платья', folder: 'v2020'},
+        {name: 'v2020_003', data: 'id="v2020_003"', description: '170$', descriptionStyle: 'Свадебные платья', folder: 'v2020'},
+        {name: 'v2020_004', data: 'id="v2020_004"', description: '180$', descriptionStyle: 'Свадебные платья', folder: 'v2020'},
+        {name: 'v2020_005', data: 'id="v2020_005"', description: '180$', descriptionStyle: 'Свадебные платья', folder: 'v2020'},
+    ],
+};
+
+const bodyEl = document.querySelector('body');
+const catalogEl = document.querySelector('.catalog__list');
+bodyEl.addEventListener('click', directoryActivation);
+
+function directoryActivation (evt) {
+    const isCollectionEl = evt.target.classList.contains('active-element');
+    if (!isCollectionEl) {
+        return;
+    }
+    let nameArray = evt.target.dataset.id;
+    for (const category in categories) {
+        if (category === nameArray) {
+            createDirectory(categories[category]);
+        }
+    }
+}
+function createDirectory (models) {
+    const liEl = models.map( models => {
+        return `
+                <li class="catalog__item">
+                    <div class="catalog__wrapper-img">
+                        <a class="catalog__link">
+                            <img 
+                            class="catalog__img active-element" 
+                            src="./images/${models.folder}/${models.name}.jpg" 
+                            alt="photo"
+                            data-${models.data}
+                            />
+                        </a>
+                    </div>
+                    <div class="catalog__description-wrapper">
+                        <p>${models.description}</p>
+                        <div class="catalog__description-border">
+                            <div class="catalog__description catalog__active">
+                                ${models.descriptionStyle}
+                            </div>
+                        </div>
+                    </div>
+                </li>
+              `;
+    }).join('');
+    catalogEl.innerHTML = liEl;
+    checkActivityBackdropMenu();
+}
+function createMainGallery () {
+    const liEl = categories.mainGallery.map(objectGallery => {
+        return `
+            <li class="main-gallery__item">
+                <div class="main-gallery__wrapper-img">
+                    <a class="main-gallery__link">
+                        <img 
+                            class="main-gallery__img active-element" 
+                            src="./images/${objectGallery.folder}/${objectGallery.name}.jpg" 
+                            alt="photo"
+                            data-${objectGallery.data}
+                        />
+                    </a>
+                </div>
+            </li>
+        `;
+    }).join('');
+    catalogEl.innerHTML = liEl;
+    setMainGalleryEl();
+}
+let iq = 0;
+let ia = 0;
+function setMainGalleryEl () {
+    if ( iq >= 1) {ia = iq;}
+    iq++;
+    const mainGalleryEl = document.querySelectorAll('.main-gallery__item');
+    const arrayMainGalleryEl = [...mainGalleryEl];
+    const lengthEl = arrayMainGalleryEl.length;
+    checkLengthEl(ia, arrayMainGalleryEl, lengthEl);
+}
+function checkLengthEl (is, arrayCheckEl, lengthEl) {
+    if (is < lengthEl) {
+        arrayCheckEl.map(arrayEl => arrayEl.classList.remove('absolute-opacity'));
+        arrayCheckEl[is].classList.add('absolute-opacity');
+        setTimeout(setMainGalleryEl, 2000);
+    } else {
+        arrayCheckEl[is-1].classList.add('absolute-opacity');
+    }
+}
+
+createMainGallery();
+
+const burgerMenuEl = document.querySelector('.header__wrapper-menu');
+const closeMenuEl = document.querySelector('.backdrop-menu__wrapper');
+const backdropMenuEl = document.querySelector('.backdrop-menu');
+burgerMenuEl.addEventListener('click', isActiveMenu);
+closeMenuEl.addEventListener('click', isActiveMenu);
+
+function isActiveMenu () {
+    backdropMenuEl.classList.toggle('is-active');
+}
+
+function checkActivityBackdropMenu () {
+    backdropMenuEl.classList.remove('is-active');
+}
+
+const searchEl = document.querySelector('.header__wrapper-loupe');
+searchEl.addEventListener('click', createSearch);
+function createSearch () {
+    catalogEl.innerHTML = `
+        <div class="search">
+            <div class="search__wrapper">
+                <input class="search__input" type="text" autofocus placeholder="Введите код модели"/>
+                <button class="search__btn">Найти</button>
+            </div>
+            <div>
+                <ul class="search__result"></ul>
+            </div>
+        </div>
+    `;
+}
+
+catalogEl.addEventListener('click', getPhotoNumber);
+function getPhotoNumber (evt) {
+    const isEl = evt.target.classList.contains('search__btn');
+    if (!isEl) {
+        return;
+    }
+    const searchResultEl = document.querySelector('.search__result');
+    const inputValueEl = document.querySelector('.search__input');
+    const inpValue = inputValueEl.value;
+    let categoryArray = inpValue[0]+inpValue[1]+inpValue[2]+inpValue[3]+inpValue[4];
+    for (let category in categories) {
+        if (category === categoryArray) {
+            let categoryArray = categories[category];
+            createPhotoDescription(categoryArray, inpValue);
+        }
+    }
+}
+function createPhotoDescription (categoryArray, inpValue) {
+    const searchResultEl = document.querySelector('.search__result');
+    const resultEl = categoryArray.map(createElement => {
+        if (createElement.name === inpValue) {
+            return `
+                <li class="full-description__item">
+                    <div class="full-description__wrapper">
+                        <a class="catalog__link">
+                            <img 
+                            class="catalog__img" 
+                            src="./images/${createElement.folder}/${createElement.name}.jpg" 
+                            alt="photo"
+                            data-${createElement.data}
+                            />
+                        </a>
+                    </div>
+                </li>
+                <li class="full-description__item">
+                    <div class="full-description__wrapper">
+                        <p class="full-description__text">Номер модели - ${createElement.name}</p>
+                        <p class="full-description__text">Из колекции - ${createElement.folder}</p>
+                        <p class="full-description__text">Цена - ${createElement.description}</p> 
+                    </div>
+                </li>
+            `;
+        }
+    }).join(''); 
+    searchResultEl.innerHTML = resultEl;
+}
